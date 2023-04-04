27c2aaa8265f763defe795f5e7f8822f0d5c0d2b
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-04-04 15:33:05 +0800

    chrome_options.add_argument added

--- Rbubbleuploader/views.py
+++ Rbubbleuploader/views.py
@@ -893,9 +893,10 @@ def Publish (request):
 def Sample_selenium(request):
     photos = Photo.objects.all()
 
-    chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     # chrome_options.add_argument("--disable-gpu")
     browser = webdriver.Chrome(options=chrome_options)
     # try:
